ffde90736a710bfb74bf93d5ab1a7ee2c3ef3d92
MPODESTA5 <mrtnpodesta@gmail.com>
2022-08-04 03:33:07 -0300

    first homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,14 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var hh = num.split("");
+  var decimal = 0;
+  for (var i = 0; i < hh.length; i++) {
+      decimal += hh[i] * Math.pow (2, (hh.length - 1 - i));
+    }
+  return decimal;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  // tu codigo a
+var hh = [];
+while (num > 1) {
+  hh.push(num % 2)
+  num = Math.floor(num / 2)
 }
-
+hh.push(num)
+return hh.reverse().join("");
+}
 
 module.exports = {
   BinarioADecimal,
